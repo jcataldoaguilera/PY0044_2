94f76c4feb563cffccc7e18ff3b4e83955883511
Juan <jcataldoaguilera@gmail.com>
2024-02-23 23:21:38 -0300

    update clase 1 js

---
+++ M2/JAVASCRIPT/ejercicio1/assets/js/script.js
@@ -0,0 +1,9 @@
+var nombre = document.getElementById("nombre");
+var apellido = document.getElementById("apellido");
+var profesion = document.getElementById("profesion");
+var salario = document.getElementById("salario");
+
+nombre.innerHTML = prompt("Ingresa tu nombre:", "")
+apellido.innerHTML = prompt("Ingresa tu apellido:", "")
+profesion.innerHTML = prompt("Ingresa tu profesion:", "")
+salario.innerHTML = prompt("Ingresa tu salario:", "")
