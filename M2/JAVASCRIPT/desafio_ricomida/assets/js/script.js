e9066c65f50674e0e8e4048ffd48d4a12cfcb682
Juan <jcataldoaguilera@gmail.com>
2024-02-28 16:44:05 -0300

    update desafio ricomida

--- M2/JAVASCRIPT/desafio_ricomida/assets/js/script.js
+++ M2/JAVASCRIPT/desafio_ricomida/assets/js/script.js
@@ -1,6 +1,16 @@
 $(document).ready(function (){
     /* Tooltip correo y favoritos */
     $('[data-toggle="tooltip"]').tooltip();
-    
-    
+    /* Alerta al clickear */
+    $('#enviarCorreo').on("click", function (){
+        alert("El correo fue enviado correctamente...")
+    });
+    /* Color rojo a INGREDIENTES y PREPARACION al hacer dblclick */
+    $('h4').on("dblclick", function (){
+        $(this).css("color", "red")
+    });
+    /* Ocultar card-text al hacer click sobre el titulo de las tarjetas */
+    $('.card-title').on("click", function (){
+        $('.card-text').toggle()
+    });
 });
