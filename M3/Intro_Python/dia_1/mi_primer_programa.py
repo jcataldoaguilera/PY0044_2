04fa4e6f809bfaadc63a3cbecc1324ebc31c78da
Juan Cataldo <jcataldoaguilera@gmail.com>
2024-03-07 23:56:51 -0300

    clase día 1

---
+++ M3/Intro_Python/dia_1/mi_primer_programa.py
@@ -0,0 +1,41 @@
+#string-> cadena de caracteres (se escribe entre comillas)
+nombre = "Mijail"
+apellido1="Palma"
+apellido2="Loren"
+edad="1"
+año=2024 #es un numero
+#Python no permite sumar letras con numeros
+#suma de string = concatenar
+print("****** Informacion *********")
+print("Tu nombre es " + nombre )
+print("Tu apellido paterno es " + apellido1 )
+print("Tu apellido materno es " + apellido2 )
+print("Tu tienes la edad de       " + edad )
+print("El año es",año," mes ",3," dia ",7)
+print("****************************")
+
+edad=18
+direccion='parcela 6'
+edad = 21.5
+edad = "50"
+
+# Interpolacion de cadenas
+
+mes = 3
+dia = 7
+
+print(f"El año es {año} del mes {mes} y el día {dia}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
