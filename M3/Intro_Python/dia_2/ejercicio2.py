2ede007d208fd038c7d6b2440877bab867bb8872
Juan <jcataldoaguilera@gmail.com>
2024-03-08 18:33:12 -0300

    dia 2 + string.format

---
+++ M3/Intro_Python/dia_2/ejercicio2.py
@@ -0,0 +1,25 @@
+# la separacion decimal de los float es un punto (.)
+# la division entre int resulta en un float
+
+print(4/2) # 2.0
+print(5/2) # 2.5
+print(5/3.5) # 1.4285714285714286
+print(2.4*4) # 9.6
+
+name = "Mijail"
+year = "2024"
+
+print(3*name) # repite 3 veces la variable name: MijailMijailMijail
+print(year*2) # 20242024
+# print(name/2) # no se puede dividir un str
+
+# interpolacion de cadenas
+month = 3
+day = 7
+
+print(f"Hola {name}, el año es {year} del mes {month} y el día {day}")
+
+# string.format
+print("".format())
+
+print("Hola {}, el año es {} del mes {} y el día {}".format(name, year, month, day))
