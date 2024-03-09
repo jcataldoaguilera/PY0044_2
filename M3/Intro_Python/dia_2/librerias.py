6ccd216d270286655de7b060008090b59b64386f
Juan <jcataldoaguilera@gmail.com>
2024-03-08 21:03:35 -0300

    librerias update

--- M3/Intro_Python/dia_2/librerias.py
+++ M3/Intro_Python/dia_2/librerias.py
@@ -1,7 +1,7 @@
 # librerias (extensiones no disponibles de forma nativa)
 import os
-# import math
-from math import sqrt
+import math
+# from math import sqrt # Importacion especifica de una libreria
 
 # operadores matematicos adicionales
 print(2**4)     # exponenciacion -> 2 elevado a 4
@@ -9,5 +9,6 @@ print(5 // 2)   # division, retorna int
 print(5/2)      # division, retorna float
 print(5%2)      # modulo, el resto de la division
 
-print(os.getcwd())  # pwd
-print(sqrt(9)) # calcula la raiz cuadrada, devuelve float
+print(os.getcwd())          # pwd
+print(math.sqrt(9))         # calcula la raiz cuadrada, devuelve float
+print(math.ceil(3.1416))    # aproxima al entero superior
