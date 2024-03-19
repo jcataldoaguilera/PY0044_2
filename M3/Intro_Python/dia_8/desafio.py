73567c76e75179595dbf2a12b75bc5329a5ba7a7
Juan Cataldo <jcataldoaguilera@gmail.com>
2024-03-18 22:13:46 -0300

    update dia 8 desafio en clase

---
+++ M3/Intro_Python/dia_8/desafio.py
@@ -0,0 +1,14 @@
+min_sn = [120, 50, 600, 30, 90, 10, 200, 0, 500]
+cad_sn = []
+
+for i in range(len(min_sn)) :
+    if min_sn[i] < 90 :
+        cad_sn.append("bien")
+    else :
+        cad_sn.append("mal")
+print(cad_sn)
+
+
+## COMPREHENSION ##
+cad_sn = ['bien' if min_sn[i] <90 else 'mal' for i in range(len(min_sn))]
+print (cad_sn)
