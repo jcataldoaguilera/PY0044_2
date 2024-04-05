60d83ced86e7b00dc8d8fa642bb885e7d514bb48
Juan Cataldo Aguilera <jcataldoaguilera@gmail.com>
2024-04-05 17:29:35 -0300

    update clases

--- M4/dia_1/pelota.py
+++ M4/dia_1/pelota.py
@@ -1,11 +1,24 @@
-class Pelota(): # definicion de clase
-    posiciones = [3, 0, 2, 1, 0] # atributos de clase
-    
-    @staticmethod # decorador para metodos estáticos
-    def crear_rebote(): # definicion de método estático
-        return [5, 0, 4, 0, 3, 0, 2, 0, 1, 0]
-    
+class Pelota():
+    #atributos
+    #pass
+    forma = "redondeada"
+    posiciones = [3,0,2,1,0]
+
+    #Metodo estatico
+    #es invocado sin tener que crear una instancia 
+    #no se pueden modificar los atributos
+    @staticmethod 
+    def crear_rebote():
+        posiciones = [5,0,4,0,3,0,2,0,1,0]
+        return posiciones
+
     @staticmethod
     def imprimir_posiciones():
         Pelota.crear_rebote()
         print(Pelota.posiciones)
+
+    #metodo NO ESTATICO
+    def rebotar(self):
+        #self.posiciones = Pelota.crear_rebote()
+        self.posiciones = self.crear_rebote()
+        self.forma = "redonda"
