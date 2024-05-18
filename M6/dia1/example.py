4d2df9b7dac74fdd9acb3a36625b792f350359a4
Juan Cataldo Aguilera <jcataldoaguilera@gmail.com>
2024-05-17 21:19:38 -0400

    dia 2

---
+++ M6/dia1/example.py
@@ -0,0 +1,9 @@
+''' Habilitar un entorno virtual en python
+python -m venv vdjango
+activamos el entorno virtual con el script activate
+instalamos django
+creamos nuevo proyecto django
+django-admin startproject mysite
+python manage.py startapp polls
+python myfirstapp\manage.py runserver
+'''
--- M6/ex1/example.py
+++
@@ -1,7 +0,0 @@
-# Habilitar un entorno virtual en python
-# python -m venv vdjango
-# activamos el entorno virtual con el script activate
-# instalamos django
-# creamos nuevo proyecto django
-# django-admin startproject myfirstapp
-# python myfirstapp\manage.py runserver
