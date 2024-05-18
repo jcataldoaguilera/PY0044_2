252791189a10a9b770299edc189fd011e6d0816d
Juan Cataldo Aguilera <jcataldoaguilera@gmail.com>
2024-05-18 10:55:36 -0400

    update comandos

--- M6/dia1/example.py
+++
@@ -1,9 +0,0 @@
-''' Habilitar un entorno virtual en python
-python -m venv vdjango
-activamos el entorno virtual con el script activate
-instalamos django
-creamos nuevo proyecto django
-django-admin startproject mysite
-python manage.py startapp polls
-python myfirstapp\manage.py runserver
-'''
