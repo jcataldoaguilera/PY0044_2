c80eaa45ca4fbe92116a4de0264a66ac8574b58c
Juan Cataldo Aguilera <jcataldoaguilera@gmail.com>
2024-05-27 18:31:12 -0400

    update

--- M6/dia1/commands.py
+++ M6/dia1/commands.py
@@ -6,6 +6,7 @@ activamos el entorno virtual con el script activate
 instalamos django
 creamos nuevo proyecto django
     django-admin startproject mysite
+    python manage.py createsuperuser
     python manage.py startapp polls
     python manage.py runserver
 
